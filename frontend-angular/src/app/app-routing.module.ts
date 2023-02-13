300b6f353ab42cacb5d301b691244d4ce370e0a0
prabhatk579 <prabhatk579@gmail.com>
2023-02-13 17:39:27 +0530

    modified:   src/app/app-routing.module.ts

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
 import { PollPageComponent } from './admin/poll-page/poll-page.component';
